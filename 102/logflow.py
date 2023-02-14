a8f6ad6e9280384af36bcd9ec2badcbb65a6d1d3
Jeff Hale <jeffmshale@gmail.com>
2023-02-14 15:55:28 -0500

    102 logging flow update

--- 102/logflow.py
+++ 102/logflow.py
@@ -2,7 +2,7 @@ from prefect import flow, get_run_logger
 
 
 @flow(name="log-example-flow")
-def log_it(name):
+def log_it():
     logger = get_run_logger()
     logger.info("INFO level log message.")
     logger.debug("You only see this message if the logging level is set to DEBUG. 🙂")
